346b0145153070aca6a7539e528651516a6f512c
Ibrahim Hosseini <sehosseini@me.com>
2024-04-22 15:56:55 +0330

    Initial Commit

---
+++ iOSBaseProject/iOSBaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBaseProjectApp.swift
+//  iOSBaseProject
+//
+//  Created by Ibrahim on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSBaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
