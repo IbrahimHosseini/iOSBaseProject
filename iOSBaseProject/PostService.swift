c1b77b0b7e970e05501fa686cd9759c15d266ac6
Ebrahim Hosseini <sehosseini@me.com>
2024-04-27 23:57:35 +0330

    add post service

--- iOSBaseProject/PostService.swift
+++ iOSBaseProject/PostService.swift
@@ -5,4 +5,22 @@
 //  Created by Ibrahim Hosseini on 4/27/24.
 //
 
-import Foundation
+import Combine
+
+protocol PostService {
+    func getPost() -> AnyPublisher<[Post], Error>
+}
+
+class PostServiceImp: PostService {
+
+    private let network: URLSessionAPIClient<PostAPIs>
+
+    init(network: URLSessionAPIClient<PostAPIs>) {
+        self.network = network
+    }
+
+    func getPost() -> AnyPublisher<[Post], Error> {
+        network.request(PostAPIs.getPosts)
+    }
+
+}
