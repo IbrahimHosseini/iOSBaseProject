8b0b82b2b1718c50062d52486e48d7549a8f2a74
Ebrahim Hosseini <sehosseini@me.com>
2024-04-27 23:57:53 +0330

    add post view model

--- iOSBaseProject/PostViewModel.swift
+++ iOSBaseProject/PostViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Combine
+
+@MainActor
+class PostViewModel: ObservableObject {
+    @Published var error: String?
+    @Published private(set) var posts = [Post]()
+
+    private var cancellable = Set<AnyCancellable>()
+
+    private var postService: PostService
+
+    init(postService: PostService) {
+        self.postService = postService
+    }
+
+    func fetchPost() {
+        postService.getPost()
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { error in
+                print(error)
+            }, receiveValue: { posts in
+                self.posts = posts
+            })
+            .store(in: &cancellable)
+    }
+}
