cebd2b3309cd6bd8ee73ad5e04a90d4ea9103659
Ibrahim Hosseini <sehosseini@me.com>
2024-05-09 10:20:07 +0330

    docs: update docs

--- iOSBaseProject/Post.swift
+++ iOSBaseProject/Post.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/**
+ *  Post struct represents a single post in the application.
+ *
+ *  This struct conforms to Codable protocol, which allows it to be easily
+ **/
 struct Post: Codable, Identifiable {
     let userId: Int
     let id: Int
