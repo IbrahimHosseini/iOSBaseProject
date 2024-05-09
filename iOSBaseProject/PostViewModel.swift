025a5a606578622407210172c94eb18b7b49fd4c
Ibrahim Hosseini <sehosseini@me.com>
2024-05-09 10:36:57 +0330

    docs: update the post view model docs

--- iOSBaseProject/PostViewModel.swift
+++ iOSBaseProject/PostViewModel.swift
@@ -8,25 +8,40 @@
 import Foundation
 import Combine
 
-@MainActor
+/// PostViewModel class is responsible for managing the data related to posts.
+///  It uses Combine framework to handle asynchronous data fetching and updates.
 class PostViewModel: ObservableObject {
+    
+    /// A published property that holds the error message if any error occurs during data fetching.
     @Published var error: String?
+    
+    /// A published property that holds the array of Post objects.
     @Published private(set) var posts = [Post]()
 
+    /// A set that holds the cancellable objects.
     private var cancellable = Set<AnyCancellable>()
 
+    /// A dependency injection of PostService instance.
     private var postService: PostService
 
+    /// Initializes the PostViewModel instance with the provided PostService instance.
+    ///
+    ///  - Parameter postService: An instance of PostService class.
     init(postService: PostService) {
         self.postService = postService
     }
 
+    /// Fetches the posts from the server and updates the `posts` property.
+    @MainActor
     func fetchPost() {
+        // Fetches the posts from the server using the postService instance.
         postService.getPost()
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { error in
                 print(error)
             }, receiveValue: { posts in
+                
+                // Updates the `posts` property with the received posts.
                 self.posts = posts
             })
             .store(in: &cancellable)
